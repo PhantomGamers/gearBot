88c70442642aed248a4641e7ab0191b2f1ea1cd7
n0tj <jay.lansiquot@icloud.com>
2019-03-31 01:44:30 -0400

    actually fixed the stupid "Bad URL, please try a different one." bug

--- cogs/gear.py
+++ cogs/gear.py
@@ -14,45 +14,44 @@ class gear_Cog(commands.Cog, name="Owner Commands"):
     async def gear(self, ctx, args):
         user = ctx.author
         str_user = str(user)
-
-            
+  
         if args.startswith("<"):
             getTag = discord.utils.get(
                         ctx.message.guild.members, id=ctx.message.raw_mentions[0])
             w = await db_sessions.sql_name(str(getTag))
             if str(getTag) == w:
-                        #Check to see weather they can have a direct link or not and then chooses the format based on that.
-                        some_list = [await db_sessions.sql_link(str(getTag))]
-                        bad = ['.jpg', '.png']
-                        flag = 0
-                        for s in some_list:
-                            for item in bad:
-                                if item in s:
-                                    flag = 1
-                        if flag == 0:
-                                #Legacy layout
-                                print(("User mention: {}").format(str(ctx.author.mention)))
-                                await ctx.send("{} 's gear: {}".format("@" + str(getTag), await db_sessions.sql_link(str(getTag))))
-                                #Logging
-                                await logger.bigLog.log_5(ctx,str_user,str(getTag))
-                        else:
-                            #Fancy frame for displaying user gear, ap and dp.
-                            embed = discord.Embed()
-                            embed.set_image(url=await db_sessions.sql_link(str(getTag)))
-                            embed.set_thumbnail(url=getTag.avatar_url)
-                            embed.set_author(name=str(getTag), icon_url=getTag.avatar_url)
-                            embed.set_footer(text="n0tj#6859 with bugs", icon_url= "https://pbs.twimg.com/profile_images/1111417292955381761/z18vzMwY_400x400.png")
-                            await ctx.send( embed=embed)
+                #Check to see weather they can have a direct link or not and then chooses the format based on that.
+                some_list = [await db_sessions.sql_link(str(getTag))]
+                bad = ['.jpg', '.png']
+                flag = 0
+                for s in some_list:
+                    for item in bad:
+                        if item in s:
+                            flag = 1
+                if flag == 0:
+                        #Legacy layout
+                        print(("User mention: {}").format(str(ctx.author.mention)))
+                        await ctx.send("{} 's gear: {}".format("@" + str(getTag), await db_sessions.sql_link(str(getTag))))
                         #Logging
-                            await logger.bigLog.log_6(ctx,str_user,str(getTag))
-                            #print(user.mentioned_in(message))
-                            #print(ctx.message.raw_mentions) #this returns the mentions ID.
-                            #print(ctx.message.guild.name) # Returns the guild that user used the command in. Should also add a timestamp when this is put in a database. So they can only change guild every 48hrs
-                            #print("\n".join([x.name for x in role.members])) #LIST OF ALL THE USERS IN A ROLE
+                        await logger.bigLog.log_5(ctx,str_user,str(getTag))
+                else:
+                    #Fancy frame for displaying user gear, ap and dp.
+                    embed = discord.Embed()
+                    embed.set_image(url=await db_sessions.sql_link(str(getTag)))
+                    embed.set_thumbnail(url=getTag.avatar_url)
+                    embed.set_author(name=str(getTag), icon_url=getTag.avatar_url)
+                    embed.set_footer(text="n0tj#6859 with bugs", icon_url= "https://pbs.twimg.com/profile_images/1111417292955381761/z18vzMwY_400x400.png")
+                    await ctx.send( embed=embed)
+                #Logging
+                    await logger.bigLog.log_6(ctx,str_user,str(getTag))
+                    #print(user.mentioned_in(message))
+                    #print(ctx.message.raw_mentions) #this returns the mentions ID.
+                    #print(ctx.message.guild.name) # Returns the guild that user used the command in. Should also add a timestamp when this is put in a database. So they can only change guild every 48hrs
+                    #print("\n".join([x.name for x in role.members])) #LIST OF ALL THE USERS IN A ROLE          
             else:
                 await ctx.send("User {} isn't in the database.".format(str(getTag)))
-        else:
-            await ctx.send("Bad URL, please try a different one.")
+                print("User {} isn't in the database.".format(str(getTag)))
+
 
 
         #This is some santization of input, when the user passes a link it verifies it is a link by checking to see if its starts with 'http'
@@ -67,7 +66,9 @@ class gear_Cog(commands.Cog, name="Owner Commands"):
                 await db_sessions.sql_new_user(str(user), args, "<@" + str(ctx.author.id) + ">")
                 await ctx.send("You have been added to the database.")
                 #Logging
-                await logger.bigLog.log_2(ctx,str_user)    
+                await logger.bigLog.log_2(ctx,str_user)
+        else:
+            await ctx.send("Invalid URL, please try a different one.")
                    
 
 
