af7ba9dba21d5edf49fdf1b7988782e508c5bb73
n0tj <jay.lansiquot@icloud.com>
2019-04-10 16:34:23 -0400

    fixed up the formatting in !gear error and !gearhelp

--- cogs/gearhelp.py
+++ cogs/gearhelp.py
@@ -13,15 +13,15 @@ class gearhelp_Cog(commands.Cog, name="Owner Commands"):
     @commands.command(name='gearhelp')
     async def gearhelp(self, ctx):
         """!gearhelp for a nicer view of the help commands."""
-        embed = discord.Embed(title="Join the gearbot support channel", colour=discord.Colour(0xa9219b), url="https://discord.gg/jZAJ7Yy")
+        embed = discord.Embed(colour=discord.Colour(0xa9219b))
 
         #embed.set_image(url="https://cdn.discordapp.com/embed/avatars/0.png")
-        embed.set_thumbnail(url="https://pbs.twimg.com/profile_images/1111417292955381761/z18vzMwY_400x400.png")
-        embed.set_author(name="n0tj#6859", url="https://n0tj.com", icon_url="https://pbs.twimg.com/profile_images/1111417292955381761/z18vzMwY_400x400.png")
+        embed.set_thumbnail(url="https://pbs.twimg.com/media/DIF3WFMVwAA1qAN.png")
+        embed.set_author(name="Join the gearBot discord", url="https://discord.gg/jZAJ7Yy", icon_url="https://pbs.twimg.com/media/DIF3WFMVwAA1qAN.png")
         embed.set_footer(text="Message n0tj#6859 with any bugs or concerns.", icon_url="https://pbs.twimg.com/profile_images/1111417292955381761/z18vzMwY_400x400.png")
 
-        embed.add_field(name="Updating your gear screenshot, direct link only", value="**!gear <link>**")
-        embed.add_field(name="Looking up someone or your own gear", value="**!gear <@user>**")
+        embed.add_field(name="**Updating your gear screenshot, direct link only**", value="**!gear <link>**")
+        embed.add_field(name="**Looking up someone or your own gear**", value="**!gear <@user>**")
 
 
         await ctx.send(embed=embed)
