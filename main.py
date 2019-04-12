86be8fd860bb63e79de84240d9669bb3a888fac4
n0tj <jay.lansiquot@icloud.com>
2019-04-12 11:01:56 -0400

    added dbl api key and fixed the dbl_api

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ sys.tracebacklimit = 0
 
 # Below cogs represents our folder our cogs are in. Following is the file name. So 'meme.py' in cogs, would be cogs.meme
 # Think of it like a dot path import
-initial_extensions = ['cogs.gear','cogs.dbl','cogs.gearhelp','cogs.errors']
+initial_extensions = ['cogs.gear','cogs.dbl_api','cogs.gearhelp','cogs.errors']
 
 bot = commands.Bot(command_prefix='!', description='gearBot')
 
